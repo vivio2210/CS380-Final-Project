7f54bccc333f2ce9703447d60c42728263a7014f
kuguardt <kuguardt@gmail.com>
2024-04-10 00:12:26 -0700

    test1

---
+++ Assets/Script/Gridpos.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gridpos : MonoBehaviour
+{
+    public int posx, posy;
+    public int x, y;
+    public int width, height;
+
+}
